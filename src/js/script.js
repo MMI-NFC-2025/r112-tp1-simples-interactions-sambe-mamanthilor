c1cca77fee0e0b16f677d163a57df870369268ec
sambe-mamanthilor <maman_thilor.sambe@edu.univ-fcomte.fr>
2025-11-07 11:14:02 +0100

    click red

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,8 @@
 const aMettreEnRouge=document.querySelector("#a-mettre-en-rouge");
 aMettreEnRouge.style.color= "red";
 const collH2= document.querySelectorAll("h2");
+collH2.forEach((elm) =>{
+  elm.addEventListener('click', (evt) => {
+    evt.target.style.color= "red";
+  });
+});
